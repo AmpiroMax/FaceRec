1332fbd83413d1d4ac9790ed9db32e40ad392ca6
AmpiroMax <patrat2015@yandex.ru>
2023-04-09 17:50:40 +0300

    Modules with models arch were added

---
+++ src/models/inference_model.py
@@ -0,0 +1,56 @@
+
+
+import albumentations as albu
+import numpy as np
+import torch
+from torch import nn
+
+MARGIN = 1
+
+
+class FaceRecModel:
+    def __init__(
+        self,
+        model: nn.Module,
+        basic_transforms: albu.BaseCompose,
+        augmentation_transforms: albu.BaseCompose
+    ) -> None:
+        self.model = model
+        self.model.cpu().eval()
+        self.basic_transforms = basic_transforms
+        self.augmentation_transforms = augmentation_transforms
+        self.emb_to_mean_num = 50
+        self.margin = 0
+        self.margin_std = 0
+        self.owner_embeddings = None
+
+    def learn_owner(self, img: np.ndarray) -> None:
+        mean_embedding = self.model(
+            self.basic_transforms(image=img)["image"][None, ...]
+        )
+
+        for _ in range(self.emb_to_mean_num):
+            mean_embedding += self.model(
+                self.augmentation_transforms(image=img)["image"][None, ...]
+            )
+
+        self.owner_embeddings = mean_embedding / self.emb_to_mean_num
+
+        margins = []
+        for _ in range(self.emb_to_mean_num):
+            margins += [
+                torch.norm(
+                    self.owner_embeddings - self.model(
+                        self.augmentation_transforms(
+                            image=img)["image"][None, ...]
+                    )
+                ).detach().numpy()
+            ]
+        self.margin = np.mean(margins)
+        self.margin_std = np.std(margins)
+
+    def recognize_face(self, img: np.ndarray) -> int:
+        img = self.basic_transforms(image=img)["image"][None, ...]
+        embedding = self.model(img)
+        return torch.norm(self.owner_embeddings - embedding) < \
+            self.margin + MARGIN + 3 * self.margin_std
